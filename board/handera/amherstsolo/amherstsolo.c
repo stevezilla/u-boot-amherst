341c8786c0c4f1dbe719d052faf4b905d1a35419
Steven Scheidecker <displaypress@ubuntu.(none)>
2015-03-30 09:26:33 -0500

    Update

--- board/handera/amherstsolo/amherstsolo.c
+++ board/handera/amherstsolo/amherstsolo.c
@@ -146,7 +146,7 @@ static int setup_fec(void)
 	mxc_iomux_set_gpr_register(1, 21, 1, 1);
 
         /* clear gpr1[14], gpr1[18:17] to select anatop clock */
-        clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC_MASK, 0);
+        /*clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC_MASK, 0); */
 
         ret = enable_fec_anatop_clock(ENET_50MHz);
         if (ret)
